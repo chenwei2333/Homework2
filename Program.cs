2daf6458ccb27fd9a9e80c36820d2b62944dc4d9
chenwei2333 <61378089+chenwei2333@users.noreply.github.com>
2020-05-30 15:28:07 +0800

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,38 +1,29 @@
 ﻿using System;
-
-
-
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SECOND2
+namespace THIRD3
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int[] array1 = new int[5] { 96, 93, 95, 97, 91 };
-            Console.WriteLine("最大值：" + array1.Max());
-            Console.WriteLine("平均值：" + array1.Average());
-            Console.WriteLine("最小值：" + array1.Min());
-            Console.WriteLine("所有元素的和：" + array1.Sum());
+            int[] visit = new int[101];
+            for (int i = 2; i <= 100; i++)
+            {
+                if (visit[i] == 1) { continue; }
+                for (int j = i + 1; j <= 100; j++)
+                {
+                    if (j % i == 0)
+                    {
+                        visit[j] = 1;
+                    }
+                }
+                Console.WriteLine(i);
+            }
         }
     }
 }
 
-
-
-
-
-
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
